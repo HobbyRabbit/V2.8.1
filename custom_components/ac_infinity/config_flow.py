2085aa6561bb47f99e3e7f28870de0fd7df9be47
HobbyRabbit <stephencewood@gmail.com>
2026-03-21 21:49:52 -0400

    Update config_flow.py

--- custom_components/ac_infinity/config_flow.py
+++ custom_components/ac_infinity/config_flow.py
@@ -1,20 +1,10 @@
 import voluptuous as vol
-
 from homeassistant import config_entries
 
 from .const import DOMAIN
 
 
 class ACInfinityConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-
-    async def async_step_bluetooth(self, discovery_info):
-        await self.async_set_unique_id(discovery_info.address)
-        self._abort_if_unique_id_configured()
-
-        return self.async_create_entry(
-            title=discovery_info.address,
-            data={"mac": discovery_info.address},
-        )
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
