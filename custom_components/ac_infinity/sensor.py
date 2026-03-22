3eda41737639b126d67cb756f74f8e1b07325018
HobbyRabbit <stephencewood@gmail.com>
2026-03-21 21:29:43 -0400

    Update sensor.py

--- custom_components/ac_infinity/sensor.py
+++ custom_components/ac_infinity/sensor.py
@@ -1,40 +1,36 @@
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
 from .const import DOMAIN
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
-    entities = [
-        ACInfinityTempSensor(coordinator),
-        ACInfinityHumiditySensor(coordinator),
-    ]
-
-    async_add_entities(entities)
+    async_add_entities([
+        TempSensor(coordinator),
+        HumiditySensor(coordinator),
+    ])
 
 
-class BaseSensor(SensorEntity):
-    def __init__(self, coordinator, key, name):
-        self.coordinator = coordinator
+class BaseSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator, key, name, unit):
+        super().__init__(coordinator)
         self._key = key
         self._attr_name = f"{coordinator.name} {name}"
         self._attr_unique_id = f"{coordinator.mac}_{key}"
+        self._attr_native_unit_of_measurement = unit
 
     @property
     def native_value(self):
         return self.coordinator.data.get(self._key)
 
-    async def async_update(self):
-        await self.coordinator.async_request_refresh()
 
-
-class ACInfinityTempSensor(BaseSensor):
+class TempSensor(BaseSensor):
     def __init__(self, coordinator):
-        super().__init__(coordinator, "temperature", "Temperature")
-        self._attr_native_unit_of_measurement = "°F"
+        super().__init__(coordinator, "temperature", "Temperature", "°F")
 
 
-class ACInfinityHumiditySensor(BaseSensor):
+class HumiditySensor(BaseSensor):
     def __init__(self, coordinator):
-        super().__init__(coordinator, "humidity", "Humidity")
-        self._attr_native_unit_of_measurement = "%"
+        super().__init__(coordinator, "humidity", "Humidity", "%")
